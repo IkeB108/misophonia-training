6164fff4c93858692d5aa6f30d21e5c7d45d7fe3
Ikebot <56776763+IkeB108@users.noreply.github.com>
2022-05-17 19:52:43 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,6 +30,7 @@ function preload() {
   throatclear-1 throatclear-2 throatclear-3
   yawn1 yawn2 yawn3
   `
+  completeSound = loadSound('complete.mp3')
 
   //split soundsToLoad by both spaces AND line breaks
   soundsToLoad = soundsToLoad.split(/(?:\n| )+/)
@@ -169,3 +170,38 @@ function draw() {
   }
   //no p5 canvas. very cool
 }
+
+
+
+
+
+// Set the date we're counting down to
+var countDownDate = Date.parse("t + 5 seconds").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0 && distance > -2000) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+    completeSound.play();
+    clickCheckNone();
+  }
+}, 1000);
